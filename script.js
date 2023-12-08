0556b68194f7f09fd397aa8f5eeefd26fcf9f6ec
kembak <kennethmbakken@gmail.com>
2023-12-08 22:49:43 +0100

    finalisert side 1

--- script.js
+++ script.js
@@ -41,3 +41,54 @@ let currentSlide = 0;
 
   // starter update slide funskjonen
   updateSlide();
+
+
+  // bildegalleri for reisemål
+
+    function showImage(galleryIndex, index) {
+        const gallery = document.querySelectorAll('.gallery')[galleryIndex];
+        const images = gallery.querySelectorAll('.galleribilde img');
+
+        if (index < 0) {
+            index = images.length - 1;
+        } else if (index >= images.length) {
+            index = 0;
+        }
+
+        images.forEach((image, i) => {
+            if (i === index) {
+                image.style.display = 'block';
+                image.style.width = '100%';
+            } else {
+                image.style.display = 'none';
+            }
+        });
+    }
+
+    function prevImage(galleryIndex) {
+        const gallery = document.querySelectorAll('.gallery')[galleryIndex];
+        const currentIndex = Array.from(gallery.querySelectorAll('.galleribilde img')).findIndex(img => img.style.display === 'block');
+        showImage(galleryIndex, currentIndex - 1);
+    }
+
+    function nextImage(galleryIndex) {
+        const gallery = document.querySelectorAll('.gallery')[galleryIndex];
+        const currentIndex = Array.from(gallery.querySelectorAll('.galleribilde img')).findIndex(img => img.style.display === 'block');
+        showImage(galleryIndex, currentIndex + 1);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Initialize each gallery on page load
+        const galleries = document.querySelectorAll('.gallery');
+        galleries.forEach((gallery, index) => {
+            showImage(index, 0);
+        });
+    });
+
+//javascript for kontaktskjema
+function submitContactForm() {
+  // Perform form validation here if needed
+
+  // Assuming the form is valid, you can redirect to the homepage (index.html)
+  window.location.href = 'index.html';
+}
